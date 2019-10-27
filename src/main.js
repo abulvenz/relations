4a1c803ac6aad082324a70fc5ae9a98baff98542
Andreas Eismann <andreas@eismaenners.de>
2019-10-28 00:10:44 +0100

    We'll have some fun till the clock strikes one. All objects are proxied on this branch now.

--- src/main.js
+++ src/main.js
@@ -2,86 +2,93 @@
 
 import m from 'mithril';
 import relations, { collection, relation, relationTypes } from './relations';
-import { img, li, ul } from './tags';
+import { img, li, ul, pre, span } from './tags';
 
-const state = {
+
+const a = () => {
+};
+
+const b = new Proxy(a, {
+    apply(target, thisArg, args) {
+        console.log('he')
+    }
+})
+
+console.log(b())
+
+
+const S = {
     users: [],
     groups: [],
     locations: [],
     cities: [],
 };
 
-const model = {
-    users: collection(state.users, { idField: 'name' }),
-    groups: collection(state.groups, { idField: 'name' }),
-    locations: collection(state.locations, { idField: 'name' }),
-    cities: collection(state.cities, { idField: 'name' })
+const M = {
+    users: collection(S.users, { idField: 'name' }),
+    groups: collection(S.groups, { idField: 'name' }),
+    locations: collection(S.locations, { idField: 'name' }),
+    cities: collection(S.cities, { idField: 'name' })
 };
 
 // 1 user can be member of multiple groups
-relation(model.users, '', model.groups)
+//relation(M.users, '', M.groups)
 
 // Read as: each user was born in exactly one city
-model.users.addRelation('wasBornIn', relationTypes.ONE_TO_ONE, model.cities);
+M.users.addRelation('wasBornIn', relationTypes.ONE_TO_ONE, M.cities);
 
 // Each city can be the birthplace of 0-N users
-model.cities.addRelation('isBirthplaceOf', relationTypes.ZERO_TO_N, model.users, 'wasBornIn');
+M.cities.addRelation('isBirthplaceOf', relationTypes.ZERO_TO_N, M.users, 'wasBornIn.name');
 
 // Each group was created by exactly one user
-model.groups.addRelation('wasCreatedBy', relationTypes.ONE_TO_ONE, model.users);
+M.groups.addRelation('wasCreatedBy', relationTypes.ONE_TO_ONE, M.users);
 
 // Each user can be the creator of 0-N groups
-model.users.addRelation('isCreatorOf', relationTypes.ZERO_TO_N, model.groups)
+M.users.addRelation('isCreatorOf', relationTypes.ZERO_TO_N, M.groups, 'wasCreatedBy.name')
 
-// Each group can contain 1-N users.
-model.groups.addRelation('contains', relationTypes.ONE_TO_N, model.users);
+// Each user can be member in 0-N groups, that can have M members.
+M.users.addRelation('isInGroups', relationTypes.N_TO_M, M.groups, 'contains.name');
 
-// Each user can be member in 0-N groups.
-model.users.addRelation('isInGroups', relationTypes.ZERO_TO_N, model.groups);
+// Add users
+M.users.add({ name: 'Berta' });
+M.users.add({ name: 'Anton' });
 
-model.users.add({ name: 'Anton' });
-model.users.add({ name: 'Berta' });
+// Add cities
+M.cities.add({ name: 'Kiew' });
+M.cities.add({ name: 'Curacao' });
 
-model.cities.add({ name: 'Kiew' });
-model.cities.add({ name: 'Curacao' });
+//console.log(M.users.get('Anton').wasBornIn)
 
-console.log(model.users.get('Anton').wasBornIn)
+M.users.get('Anton').wasBornIn = 'Kiew';
+M.users.get('Berta').wasBornIn = 'Kiew';
 
-model.users.get('Anton').wasBornIn = 'Kiew';
-model.users.get('Berta').wasBornIn = 'Curacao';
+console.log('Result',M.users.filterByProperty('wasBornIn.name','Kiew').map(f=>f()))
 
-console.log(model.users.get('Berta').wasBornIn)
+console.log(S)
 
-relations.use(model.users.get('Anton').wasBornIn, city => {
+
+console.log(M.users.get('Berta').wasBornIn)
+
+relations.use(M.users.get('Anton').wasBornIn, city => {
     console.log(city.name);
 });
 
-console.log(model.cities.filterBy('Amsterdam'))
-console.log(relations.first(model.cities.filterBy('Amsterdam')))
+M.groups.add({ name: 'Spökes' });
 
-console.log(model.cities.get('Amsterdam'))
-
-console.log(state.users)
-console.log(state.cities)
-console.log(state.groups)
-
-model.groups.add({ name: 'Spökes' });
-model.groups.get('Spökes').wasCreatedBy = 'Anton'
-
-console.log(model.groups.which)
-console.log(
-model.groups.which.wasCreatedBy())
+M.groups.get('Spökes').wasCreatedBy = 'Anton'
 
 m.mount(document.body, {
     view: vnode => [
         ul(
-            model.users.map(user => li(user.name, ' was born in ', user.wasBornIn))
+     //       M.users.map(user => li(user.name, ' was born in ', JSON.stringify(user.wasBornIn())))
         ),
         ul(
-            model.cities.map(city => li(city.name, ' is birthplace of ', city.isBirthplaceOf))
+            M.cities.map(city => li(city.name, ' is birthplace of ', JSON.stringify(city.isBirthplaceOf.map(f=>f()))))
         ),
         ul(
-            model.groups.map(city => li(city.name, ' was created by ', city.wasCreatedBy))
-        )
+      //      M.groups.map(group => li(group.name, ' was created by ', JSON.stringify(group.wasCreatedBy())))
+        ),
+        span(relations.use(M.groups.get('Spökes'), group => ['"', group.name, '" was created by someone from "', group.wasCreatedBy.wasBornIn.name, '"'])),
+        pre(JSON.stringify(S, null, 2))
     ]
 });
--- src/relations.js
+++ src/relations.js
@@ -31,11 +31,72 @@ const uuid = (() => {
     };
 })();
 
-export function collection(data, {
+const getProp = (path, obj) => {
+    if (!obj) return obj;
+
+    const frags = path.split('.');
+
+    if (frags.length === 1)
+        console.log('Fraggles', obj, path)
+
+    obj = obj[frags.splice(0, 1)];
+    if (frags.length > 0) {
+        return getProp(frags.join('.'), obj);
+    }
+    console.log('eat this', obj)
+
+    return obj;
+}
+
+
+const a = {
+    b: {
+        name: 'Hellou'
+    }
+}
+
+console.log(getProp('b.name', a))
+console.log(getProp('name', a.b))
+
+
+
+
+export function collection(data_, {
     idField = 'id',
     required = [],
     optional = [],
 }) {
+
+    const thisObject = {};
+
+    const handler = {
+        apply: function (obj) {
+            console.log('apply', obj())
+            return obj()
+        },
+        get: function (target, prop, receiver) {
+            //   
+            if (prop in relations) {
+                console.log('ggg', relations, prop, relations[prop])
+                return relations[prop].resolve(target());
+            } else {
+                console.log('get', target(), prop, receiver)
+                return Reflect.get(target(), prop, receiver);
+            }
+        },
+        set: function (target, prop, value) {
+            //   console.log(target, prop, value);
+            relations[prop].establish(target(), value);
+            return true;
+        },
+        ownKeys: (target)=>Object.keys(target())
+    };
+
+    const proxyObject = obj =>
+        obj && new Proxy(() => obj, handler);
+
+    const data = data_.map(proxyObject);
+
     // Array functions
     const find = fn => data.find(fn);
     const map = fn => data.map(fn);
@@ -47,8 +108,14 @@ export function collection(data, {
     // Id functions
     const idOf = element => element[idField];
     const idEquals = id => element => idOf(element) === id;
-    const propertyOf = property => element => element[property];
-    const propertyEquals = (property, value) => use(propertyOf(property), prop => prop(element) === value);
+    const propertyOf = property => element => getProp(property, element);// element[property];
+
+    const propertyEquals = (property, value) => element =>
+        use(propertyOf(property), prop => {
+            console.log('deep inside: ', property, element,value)
+            return prop(element) === value;
+        })
+
     const hasId = element => !!idOf(element);
     const hasProperty = property => element => property in element;
 
@@ -66,51 +133,31 @@ export function collection(data, {
                 !hasId(element),
                 () => element[idField] = uuid.create(),
                 () => {
-                    data.push(element);
+                    data_.push(element);
+                    data.push(proxyObject(element));
                     return idOf(element);
                 }
             );
 
     const relations = {};
 
-    const handler = {
-        apply: function (obj, thisArg, args) {
-          //  console.log(obj, prop, value)
-            return (obj[prop]
-                && Reflect.apply(obj[prop], obj, args))
-                || (relations[prop]
-                    && Reflect.apply(relations[prop].resolve, obj, args));
-        },
-        get: function (target, prop, receiver) {
-          //  console.log(target, prop, receiver)
-            if (prop in relations) {
-                return relations[prop].resolve(target);
-            } else {
-                return Reflect.get(...arguments);
-            }
-        },
-        set: function (target, prop, value) {
-         //   console.log(target, prop, value);
-            relations[prop].establish(target, value);
-            return true;
-        }
-    };
+    const get = id => use(filterBy(id), result => first(result));
 
-    const proxyObject = obj => {
-       // console.log(obj)
-        return obj && new Proxy(obj, handler);
-    }
-
-    const get = id => proxyObject(use(filterBy(id), result => first(result)));
-
-    const addRelation = (name, type, collection) => {
+    const addRelation = (name, type, collection, foreignKey) => {
         if (!!relations[name])
             fail(`Relation was already defined: ${name}`);
-        relations[name] = relation(name, collection);
-     //   console.log(relations)
+        if (type === relationTypes.ONE_TO_ONE)
+            relations[name] = relation(name, collection);
+        else if (type === relationTypes.ZERO_TO_N)
+            relations[name] = relation_ZERO_TO_N(foreignKey, collection);
+        else if (type === relationTypes.ONE_TO_N)
+            relations[name] = relation(name, collection);
+        else if (type === relationTypes.N_TO_M)
+            relations[name] = relation(name, collection);
+        //   console.log(relations)
     };
 
-    return {
+    return Object.assign(thisObject, {
         get,
         add,
         find,
@@ -126,8 +173,9 @@ export function collection(data, {
         pluck,
         addRelation,
         data,
+        data_,
         which: relations
-    };
+    });
 }
 
 export default {
@@ -150,8 +198,21 @@ export const relationTypes = {
 
 export function relation(name, collection) {
     return {
-        resolve: (obj, key) => exec(() => null/*console.log('obj', obj, obj[name], collection)
-        */, () => collection.get(obj[name])),
+        resolve: (obj, key) => collection.get(obj[name]),
+        establish: (obj, value) => obj[name] = value,
+    };
+};
+
+export function relation_ZERO_TO_N(name, collection) {
+
+    console.log('creating ', name, 'for', collection.data_)
+
+    return {
+        resolve: (obj, key) => {
+            // wasBornIn Curacao collection=users
+            console.log('resolve', name, obj, collection.data_, collection.idOf(obj))
+            return collection.filterByProperty(name, collection.idOf(obj));
+        },
         establish: (obj, value) => obj[name] = value,
     };
 };
